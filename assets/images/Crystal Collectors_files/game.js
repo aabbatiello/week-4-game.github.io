4f15f36a9a70835a6d0f28ef4f503ac557deb9dd
aabbatiello <anthonyjabbatiello@gmail.com>
2017-04-19 21:35:07 -0400

    HWUpdate

--- assets/images/Crystal Collectors_files/game.js
+++ assets/images/Crystal Collectors_files/game.js
@@ -1,32 +1,31 @@
-
+//Global Variables 
 
 var wins = 0;
 var losses = 0;
+var addition = 0;
 
+
+//Assigns Random Values to Randome Number and Crystals using math floor
 var randomnumber = Math.floor((Math.random()* 100) + 19);
-
 var red = Math.floor((Math.random()* 12) + 1);
 var blue = Math.floor((Math.random()* 12) + 1);
 var yellow = Math.floor((Math.random()* 12) + 1);
 var green = Math.floor((Math.random()* 12) + 1);
 
-
-var addition = 0;
-
-
-
-
+//updates addition in html
 var updateAddition = function (){
 	$('.addition').empty();
 	$('.addition').append(addition);
-	$("#wins").html("Wins: " + wins);
-	$("#losses").html("Losses: " + losses);
+	$('#wins').empty();
+	$('#wins').append(wins);
+	$('#losses').empty();
+	$('#losses').append(losses);
 }
-
-
+//restart function
 var restart = function(){
 	addition = 0;
-    randomnumber = Math.floor((Math.random()* 100) + 19);	
+    randomnumber = Math.floor((Math.random()* 100) + 19);
+
 	$('.randomnumber').empty();
 	$('.randomnumber').append(randomnumber);
 
@@ -34,10 +33,9 @@ var restart = function(){
     blue = Math.floor((Math.random()* 12) + 1);
     yellow = Math.floor((Math.random()* 12) + 1);
     green = Math.floor((Math.random()* 12) + 1);
+    //alert('restart');
     updateAddition();
 }
-
-
 var logic = function (){
 	if (addition == randomnumber) {
 		wins ++;
@@ -52,6 +50,7 @@ var logic = function (){
 $('.randomnumber').append(randomnumber);
 
 $('.addition').append(addition);
+//function
 
 $(document).ready(function(){
 	$('#red').click(function(){
